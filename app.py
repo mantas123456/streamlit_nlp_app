0f05288d250d4a17f377ca98dda36e83893440c1
mantas123456 <mantas.valantinavicius@gmail.com>
2025-04-02 17:04:47 +0200

    Fix: install en_core_web_sm via requirements.txt for Streamlit Cloud

--- app.py
+++ app.py
@@ -8,13 +8,7 @@ from collections import Counter
 
 
 
-# Ensure spaCy model is available on Streamlit Cloud
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    from spacy.cli import download
-    download("en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+
 
 
 
